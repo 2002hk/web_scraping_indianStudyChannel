d05d3745614488e9e48bb995aed5e6950130eec4
2002hk <hrutujakalaskar@gmail.com>
2024-12-23 11:02:15 +0530

    added comment messages

--- scraper/univscraper/univscraper/spiders/univspider.py
+++ scraper/univscraper/univscraper/spiders/univspider.py
@@ -1,28 +1,32 @@
 import scrapy
 
-
-
+# spider class
 class UnivspiderSpider(scrapy.Spider):
     name = "univspider"
-    allowed_domains = ["www.indiastudychannel.com"]
-    start_urls = ["https://www.indiastudychannel.com/universities/state-13-gujarat"]
+    allowed_domains = ["www.indiastudychannel.com"] #all the allowed domains
+    start_urls = ["https://www.indiastudychannel.com/universities/state-13-gujarat"] # start to scrape from this url
 
     def parse(self, response):
-        #univs=response.xpath("//span[@id='ContentPlaceHolder1_lblUniversities']")
-        #table_rows=response.xpath('//span[@id="ContentPlaceHolder1_lblUniversities"]/table[1]').get()
+        
+
         queries=response.xpath("//span[@id='ContentPlaceHolder1_lblUniversities']/table[1]/descendant::a/@href")
+        #this xpath gives list of 20 links present in the first page
+        # we will loop through each url to extract content inside the page 
         for query in queries:
+            
             relative_univ_url=query.get()
 
-            univ_url='https://www.indiastudychannel.com'+relative_univ_url
+            univ_url='https://www.indiastudychannel.com'+relative_univ_url  
             yield response.follow(univ_url,callback=self.parse_univ_page)
         
+        # it moves to the next page using the below xpath
         next_page=response.xpath("//span[@id='ContentPlaceHolder1_lblUniversities']/table[2]//descendant::a[position()=1]/@href").get()
 
         '''for query in range(0,len(next_page)-1):
             link=query.get()
             next_page_url='https://www.indiastudychannel.com'+link
             yield response.follow(next_page_url,self.parse)'''
+        
         
         if next_page is not None:
             next_page_url='https://www.indiastudychannel.com'+next_page
